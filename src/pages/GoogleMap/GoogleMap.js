860351102db2fe136b9ad693200977d7899a8a09
benndip <obentabiayuk1@gmail.com>
2020-06-23 00:07:53 +0100

    Configured google maps

--- src/pages/GoogleMap/GoogleMap.js
+++ src/pages/GoogleMap/GoogleMap.js
@@ -1,7 +1,30 @@
-function initMap(){
-    var location= {lat:4.159302,lng:9.243536};
-    var map = new google.maps.Map(document.getElementById("map"),{
-        zoom:4,
-        center: location
+//getting user location
+function getLocation() {
+    navigator.geolocation.watchPosition(position => {
+        let array = [position.coords.longitude, position.coords.latitude];
+        console.log(array);
+        return {
+            array
+        };
     });
 }
+
+getLocation();
+
+//   display map
+mapboxgl.accessToken = "pk.eyJ1IjoiYmVubmRpcCIsImEiOiJjazRtdWExYWwweHA4M2tuNTljbmxjcjlmIn0.ji9iFK1hYN1sP1H-Kl99Rw";
+
+let map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v9",
+    center: [9.2632243, 4.1559658],
+    zoom: 15
+});
+
+map.addControl(new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true
+}));
+// var marker = new mapboxgl.Marker().setLngLat([9.2632243, 4.1559658]).addTo(map);
--- src/pages/SelectOptionPage/SelectOptionPage.js
+++ src/pages/SelectOptionPage/SelectOptionPage.js
@@ -1,31 +1,31 @@
 const data = [
     {
         id: 1,
-        title: 'Board a bus',
+        title: 'Board',
         image: '../../../res/images/bs2.jpeg',
         link: "../Home/Home.html",
         description: 'Here You can board a bus for a travell,you will have to select your region and town where you are'
     },
     {
         id: 2,
-        title: 'See Settings',
+        title: 'Settings',
         image: '../../../res/images/s3.jpeg',
         link: "../SettingsPage/SettingsPage.html",
         description: 'Go through your account settings and other settings like language, location and also premium services'
     },
     {
         id: 3,
-        title: 'Our Location',
+        title: 'Location',
         image: '../../../res/images/l1.jpeg',
-        link: "../Home/Home.html",
+        link: "../GoogleMap/GoogleMap.html",
         description: 'Know our Location and also more about us'
     },
     {
         id: 4,
-        title: 'This is title',
-        image: '../../../res/images/bd1.png',
+        title: 'Entertainment',
+        image: '../../../res/images/en.jpeg',
         link: "../Home/Home.html",
-        description: 'This is more text for the description'
+        description: 'Keep yourselt busy with some games and exciting videos and sounds'
     }
 ];
 
